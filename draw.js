e72be8f57a1fe9de3d6f0311c63f585df89bcb62
Joe Bruzek <jbruzek@MacBook-Pro.local>
2014-11-10 13:43:53 -0500

    Added HelloWorld PDF example

--- draw.js
+++ draw.js
@@ -1,45 +1,47 @@
-// When modified, copy into pdfjs/web
-'use strict'
-// var canvasDiv = document.getElementById('viewer'); //The PDF Viewer
-var context = canvas.getContext('2d');
 
-// Width of entire document
-var canvasWidth = $('pdfViewer').width();;
-var canvasHeight = $('.pdfViewer').height();;
+var canvasWidth = 800;
+var canvasHeight = 400;
 
-// canvas = document.createElement('canvas');
-var canvas = $('canvas');
-console.log(canvas);
-// canvas.setAttribute('width', canvasWidth);
-// canvas.setAttribute('height', canvasHeight);
-// canvas.setAttribute('z-index', 1000);
-// canvas.setAttribute('id', 'canvas');
-// canvasDiv.appendChild(canvas);
+
+// var pdf = document.getElementById('the-canvas');
+// pdf.setAttribute('width', canvasWidth);
+// pdf.setAttribute('height', canvasHeight);
+
+
+var canvasDiv = document.getElementById('canvasDiv');
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', canvasWidth);
+canvas.setAttribute('height', canvasHeight);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
 if(typeof G_vmlCanvasManager != 'undefined') {
   canvas = G_vmlCanvasManager.initElement(canvas);
 }
+context = canvas.getContext("2d");
 
-$('#page1').mousedown(function(e){
+$('#canvas').mousedown(function(e){
   var mouseX = e.pageX - this.offsetLeft;
   var mouseY = e.pageY - this.offsetTop;
-
+    
   paint = true;
   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
   redraw();
 });
 
-$('#page1').mousemove(function(e){
+$('#canvas').mousemove(function(e){
   if(paint){
     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
     redraw();
   }
 });
 
-$('#page1').mouseup(function(e){
+$('#canvas').mouseup(function(e){
   paint = false;
 });
 
-$('#page1').mouseleave(function(e){
+// $('#pdf').mouseup(function(){console.log($(".selection-highlight"))});
+
+$('#canvas').mouseleave(function(e){
   paint = false;
 });
 
@@ -47,31 +49,34 @@ var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
 var paint;
+var curTool = "marker";
+var size = 5;
+var shape = "round";
 
 var colorPurple = "#cb3594";
 var colorGreen = "#659b41";
-var colorYellow = "#ffcf33";
+var colorYellow = "#ffff00";
+var colorBlack = "#000000";
 
-var curColor = colorPurple;
+var curColor = colorBlack;
 var clickColor = new Array();
 
 function addClick(x, y, dragging)
 {
   clickX.push(x);
-  clickY.push(y);
+  clickY.push(y); //space for the buttons up top
   clickDrag.push(dragging);
   clickColor.push(curColor);
 }
 
 function redraw(){
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-  // ^^ Clears the canvas
-
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
   context.strokeStyle = "#df4b26";
-  context.lineJoin = "round";
-  context.lineWidth = 5;
-
-  for(var i=0; i < clickX.length; i++) {
+  context.lineJoin = shape;
+  context.lineWidth = size;
+      
+  for(var i=0; i < clickX.length; i++) {    
     context.beginPath();
     if(clickDrag[i] && i){
       context.moveTo(clickX[i-1], clickY[i-1]);
@@ -83,24 +88,62 @@ function redraw(){
      context.strokeStyle = clickColor[i];
      context.stroke();
   }
+  if(curTool == "highlighter") {
+    context.globalAlpha = 0.3;
+    context.drawImage(0, 0, canvasWidth, canvasHeight);
+  }
+  context.globalAlpha = 1;
+}
+
+function drawToggle() {
+  $('#draw').toggleClass("down");
+  $('#canvas').toggleClass("static");
 }
 
 $('.clear').click(function() {
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
   clickX = new Array();
   clickY = new Array();
   clickDrag = new Array();
   clickColor = new Array();
 });
 
+$(".bbb").click(function() {
+  console.log("this has been clicked");
+});
+
+$('.black').click(function() {
+  curColor = colorBlack;
+  curTool = "marker";
+  size = 5;
+  shape = "round";
+})
+
 $('.purple').click(function() {
   curColor = colorPurple;
+  curTool = "marker";
+  size = 5;
+  shape = "round";
 })
 
 $('.green').click(function() {
   curColor = colorGreen;
+  curTool = "marker";
+  size = 5;
+  shape = "round";
 })
 
 $('.yellow').click(function() {
   curColor = colorYellow;
+  curTool = "marker";
+  size = 5;
+  shape = "round";
 })
+
+$('.highlight').click(function() {
+  curColor = colorYellow;
+  curTool = "highlighter";
+  size = 15;
+  shape = "square";
+})
+
